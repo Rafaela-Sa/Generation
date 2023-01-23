cf97e61860f0ab304b25359e28628b21a2c3b8b9
Rafaela Soares de Sá <120683430+Rafaela-Sa@users.noreply.github.com>
2023-01-23 12:17:07 -0300

    Atualização

--- Exercícios/JAVA/Atividade 07/Atividade7.java
+++ Exercícios/JAVA/Atividade 07/Atividade7.java
@@ -59,3 +59,32 @@ public class Exercicio3 {
         System.out.println("\nO total de pessoas com idade menor que 21 anos é: " + idadeMenos + "\nO total de pessoas com idade maior que 50 anos é: " + idadeMais);
     }
 }
+
+// EXERCÍCIO 6
+
+package atividade7;
+
+import java.util.Scanner;
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+        int numero, quantidade = 0, soma = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.print("Digite um número: ");
+            numero = sc.nextInt();
+
+            if (numero < 0) {
+                System.out.println("Valor inválido!");
+                break;
+            }
+            if (numero % 3 == 0 && numero != 0) {
+                quantidade++;
+                soma += numero;
+            }
+        } while (numero != 0);
+        System.out.println("\nA média de todos os números múltiplos de 3 digitados é: " + soma / quantidade);
+    }
+}
